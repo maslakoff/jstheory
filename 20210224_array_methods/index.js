e8deb6eb2d339c59c344cf03c6bd08dd92f9f3b9
i.maslakoff <igormaslakoff@gmail.com>
2021-02-24 23:53:59 +0300

    array methods
    Co-authored-by: Dmitri Urbanovich <dimaurbanovich@users.noreply.github.com>
    Co-authored-by: rominazz <rominazz@users.noreply.github.com>
    Co-authored-by: Ivantsov-aa <Ivantsov-aa@users.noreply.github.com>
    Co-authored-by: A-Dulevich <A-Dulevich@users.noreply.github.com>
    Co-authored-by: rominaghanbari@gmail.com <rominaghanbari@gmail.com>

---
+++ 20210224_array_methods/index.js
@@ -0,0 +1,53 @@
+const flatArray = [
+    'January', 'February', 
+    'March', 'April', 
+    'May', 'June', 
+    'July', 'August', 
+    'September', 'October', 
+    'November', 'December'
+];
+
+const arrayOfObjects = [
+    { id: 16, name: 'maslakoff', repo: 'https://github.com/maslakoff/maslakoff' },
+    { id: 36, name: 'A-Dulevich', repo: 'https://github.com/A-Dulevich/cv' },
+    { id: 43, name: 'dimaurbanovich', repo: 'https://github.com/dimaurbanovich/cv' },
+    { id: 78, name: 'valevka', repo: 'https://github.com/valevka/cv' },
+    { id: 79, name: 'Ivantsov-aa', repo: 'https://github.com/Ivantsov-aa/cv' },
+    { id: 20, name: 'Elizaveta-Battalova', repo: 'https://github.com/Elizaveta-Battalova/cv' },
+    { id: 19, name: 'Darl93', repo: 'https://github.com/Darl93/cv' },
+    { id: 11, name: 'rominazz', repo: 'https://github.com/rominazz/cv' },
+]; 
+
+// // push, 
+// const newArr = flatArray.push("All year");
+// console.log(flatArray, newArr);
+
+// // unshift,
+// const arrUnshift = flatArray.unshift('Month:');
+// console.log( flatArray, arrUnshift ); 
+
+// // shift, 
+// const firstElement = flatArray.shift();
+// console.log('Первый элемент массива должен быть удалён: ',  firstElement, flatArray)
+
+// // concat, 
+// seasons = ['winter', 'spring', 'summer', 'autumn'];
+// const flatArraySeasons = flatArray.concat(seasons);
+// console.log(flatArraySeasons);
+
+// reverse,  // A-Dulevich
+// forEach,  // dimaurbanovich
+// map,      // valevka
+// filter,   // Ivantsov-aa
+// sort,     // Elizaveta-Battalova
+// indexOf,  // Darl93
+// includes, // rominazz
+
+// slice,    // A-Dulevich
+// splice,   // dimaurbanovich
+// reduce,   // valevka
+// lastIndexOf,  // Ivantsov-aa
+// find,     // Elizaveta-Battalova
+// findIndex // Darl93
+// pop,      // rominazz
+
